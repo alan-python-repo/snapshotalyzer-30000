dc030e56461e216c72aaffc913721bf6420ef40d
Alan Crisologo <acrisologo@Alans-MBP.fios-router.home>
2020-12-31 10:13:31 -0500

    Update scripts to use the click module and show more information about instances

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,8 +1,21 @@
 import boto3
+import click
+
+## moved here so everytime we run our script we have access to the session & ec2 resource
+session = boto3.Session(profile_name = 'shotty')
+ec2 = session.resource('ec2')
+
+@click.command()
+def list_instances():
+    "List EC2 instances"
+    for i in ec2.instances.all():
+        print(', '.join((
+        i.id,
+        i.instance_type,
+        i.placement['AvailabilityZone'],
+        i.state['Name'],
+        i.public_dns_name
+        )))
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name = 'shotty')
-    ec2 = session.resource('ec2')
-
-    for i in ec2.instances.all():
-        print(i)
+    list_instances()
